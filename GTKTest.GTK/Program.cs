5d5a8da3e86be955824d75cdec168b6309e51a29
Lachlan Gordon <lachlanwgordon@gmail.com>
2021-05-02 12:31:45 +1000

    added projects

---
+++ GTKTest.GTK/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gtk;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+using Application = Gtk.Application;
+
+namespace GTKTest.GTK
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            Forms.Init();
+
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("GTK TEST");
+            window.Show();
+
+            Application.Run();
+        }
+    }
+}
